5d2feee061ef62230cb6bf616c79f67e3207e4fa
dexterR35 <pusaw202@gmail.com>
2024-02-19 14:01:48 +0200

    fix the route

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-// src/App.js
-import { Routes, Route, Navigate } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import { useEffect, useState } from "react";
 import LoginPage from "./components/LoginPage/LoginPage";
 import UserDataPage from "./components/UserData/UserData";
 import { checkAuthStatus } from "./services/authService";
-import { BrowserRouter } from "react-router-dom";
+
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; // This imports default styling
 // import LoaderPage from "./components/LoadingPage";
@@ -23,50 +27,47 @@ const App = () => {
   }, []);
 
   return (
-    <>
-      {" "}
-      <BrowserRouter>
-        <ToastContainer />
-        {/* {user && <LoaderPage />}{" "} */}
-        <Routes>
-          <Route
-            path="/admin/login"
-            element={
-              user ? (
-                <Navigate to="/admin/user-data" />
-              ) : (
-                <LoginPage setUser={setUser} />
-              )
-            }
-          />
-          <Route
-            path="/admin"
-            element={
-              user ? (
-                <Navigate to="/admin/user-data" /> // If user is logged in, redirect to user data within admin
-              ) : (
-                <Navigate to="/login" /> // If not logged in, redirect to login page
-              )
-            }
-          />
-          <Route
-            path="/admin/user-data"
-            element={
-              user ? (
-                <UserDataPage /> // Only show UserDataPage if user is logged in
-              ) : (
-                <Navigate to="/admin/login" /> // If not logged in, redirect to login page
-              )
-            }
-          />
+    <Router>
+      <ToastContainer />
+      {/* {user && <LoaderPage />} */}
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            user ? (
+              <Navigate to="/user-data" />
+            ) : (
+              <LoginPage setUser={setUser} />
+            )
+          }
+        />
+        <Route
+          path="/admin"
+          element={
+            user ? (
+              <Navigate to="/user-data" /> // If user is logged in, redirect to user data within admin
+            ) : (
+              <Navigate to="/login" /> // Corrected Redirect to login page within admin
+            )
+          }
+        />
+        <Route
+          path="/user-data"
+          element={
+            user ? (
+              <UserDataPage /> // Only show UserDataPage if user is logged in
+            ) : (
+              <Navigate to="/login" /> // If not logged in, redirect to login page within admin
+            )
+          }
+        />
 
-          <Route
-            path="/*"
-            element={<Navigate to="/admin/login" />} // Redirect all undefined routes to /admin
-          />
-        </Routes>
-      </BrowserRouter>
-    </>
+        <Route
+          path="/*"
+          element={<Navigate to="/login" />} // Redirect all undefined routes to admin login
+        />
+      </Routes>
+    </Router>
   );
 };
 
