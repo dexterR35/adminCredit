43b5ba74b4e84eff0ab377d710e7fa60c137bc81
dexterR35 <pusaw202@gmail.com>
2024-04-24 11:59:49 +0300

    update lang and remove some files

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -3,19 +3,19 @@ import NavBar from '../Header/NavBar';
 import AsideMenu from './Aside/AsideMenu';
 import AsideMenuRight from './Aside/AsideMenuRight';
 
-const MainLayout = ({ children, t }) => {
+const MainLayout = ({ children }) => {
     return (
         <div className="flex flex-col">
             <NavBar />
             <div className="grid grid-cols-12 w-full">
                 <div className='col-start-1 col-end-3'>
-                    <AsideMenu t={t} /> {/* Pass t function here */}
+                    <AsideMenu />
                 </div>
                 <main className="p-2 col-start-3 col-end-11">
                     {children}
                 </main>
                 <div className='col-start-11 col-end-13'>
-                    <AsideMenuRight t={t} />
+                    <AsideMenuRight />
                 </div>
             </div>
         </div>
